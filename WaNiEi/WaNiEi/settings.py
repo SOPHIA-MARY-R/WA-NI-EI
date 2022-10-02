14a48459bd6088d6e37aeedb443c5d337f2c7512
SOPHIA MARY R <soph20410.cs@rmkec.ac.in>
2022-10-02 19:50:11 +0530

    Added Static Folder & Media Folder

--- WaNiEi/WaNiEi/settings.py
+++ WaNiEi/WaNiEi/settings.py
@@ -29,6 +29,7 @@ DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -118,6 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS=[(os.path.join(BASE_DIR,'Static'))]
+MEDIA_ROOT = os.path.join(BASE_DIR, 'Files')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
