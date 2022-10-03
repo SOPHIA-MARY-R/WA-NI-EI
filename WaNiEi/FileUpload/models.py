e52eba35845c0565a2afb1e3ba4e696572c38306
SOPHIA MARY R <soph20410.cs@rmkec.ac.in>
2022-10-03 13:16:25 +0530

    Updated FileUpload with file deletion

--- WaNiEi/FileUpload/models.py
+++ WaNiEi/FileUpload/models.py
@@ -4,8 +4,9 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class File(models.Model):
+     title = models.CharField(max_length=50)
      file = models.FileField()
      created_at = models.DateTimeField(auto_now_add=True)
      #file_of=models.ForeignKey(User, on_delete=models.CASCADE)
      def __str__(self): #function to name instances
-        return self.file
+        return self.title
--- WaNiEi/FileUpload/views.py
+++ WaNiEi/FileUpload/views.py
@@ -1,23 +1,22 @@
 from dataclasses import dataclass
-from datetime import datetime
+import datetime
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-
+from googletrans import Translator
 
 from FileUpload.models import File
-from .forms import UploadFileForm
-from django.views.generic.edit import CreateView
-from django.views.generic.edit import FormView
+#from .forms import UploadFileForm
+#from django.views.generic.edit import CreateView
+#from django.views.generic.edit import FormView
 
 def upload_file(request):
     if request.method=='POST':
-        if request.FILES.getlist('FILE'):
-            for f in request.FILES.getlist('FILE'):
-                File(file=f).save()
+        if request.FILES['FILE']:
+            myfile=request.FILES['FILE']
+            File(title=myfile.name, file=myfile, created_at=datetime.datetime.now()).save()
             return render(request, 'FileUpload/upload.html')
         else:
            return HttpResponse('Upload unsuccessful!') 
            #return redirect(request, 'FileUpload/translate')
     else:
          return render(request, 'FileUpload/upload.html')
-
