3e459f3ee18992b1bb9e10d013c201ef9ffea735
SOPHIA MARY R <soph20410.cs@rmkec.ac.in>
2022-10-04 18:25:00 +0530

    Implemented translate & translated file storage

--- WaNiEi/FileUpload/views.py
+++ WaNiEi/FileUpload/views.py
@@ -14,6 +14,21 @@ def upload_file(request):
         if request.FILES['FILE']:
             myfile=request.FILES['FILE']
             File(title=myfile.name, file=myfile, created_at=datetime.datetime.now()).save()
+            myfilepath='D:/INDIGILAB_WORKSHOP/WaNiEi/WaNiEi/Files/'+myfile.name
+            mytranslatedfilepath='D:/INDIGILAB_WORKSHOP/WaNiEi/WaNiEi/Files/translated_'+myfile.name
+            mytranslatedfile='translated_'+myfile.name
+            string=''
+            with open(myfilepath, 'r', encoding='utf-8') as f:
+                for line in f: string+=line
+            f.close()
+            Lines=string.split(' ')
+            t=open(mytranslatedfilepath, 'w')
+            for line in Lines:
+                translator=Translator()
+                translated=translator.translate(line).text
+                t.write(translated)
+            t.close()
+            File(title=mytranslatedfile, file=mytranslatedfile, created_at=datetime.datetime.now()).save()
             return render(request, 'FileUpload/upload.html')
         else:
            return HttpResponse('Upload unsuccessful!') 
