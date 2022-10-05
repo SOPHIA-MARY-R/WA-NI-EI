a31eb57eb83a47153bebd4488bd23686618ac797
SOPHIA MARY R <soph20410.cs@rmkec.ac.in>
2022-10-05 12:04:18 +0530

    Implemented download functionality

--- WaNiEi/FileUpload/urls.py
+++ WaNiEi/FileUpload/urls.py
@@ -1,9 +1,8 @@
-from django.conf import settings
-from django.conf.urls.static import static
+
 from django.urls import URLPattern, path
 from . import views
 
 urlpatterns = [
    #path('',views.history),
    path('upload', views.upload_file, name='upload_file'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] 
